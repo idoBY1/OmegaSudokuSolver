5f3eb23ef248f0ca066489e3f5696d148c59aa5a
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-01-10 11:13:15 +0200

    feat: created solution and Program class

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OmegaSudokuSolver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sudoku!");
+        }
+    }
+}
