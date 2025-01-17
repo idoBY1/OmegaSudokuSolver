cc284dccdf22de1bd6d883bc9216681eb37df13c
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-01-17 16:05:18 +0200

    feat!: started implementing backtracking solver. NOT FINISHED!

---
+++ src/Solvers/BasicBacktrackingSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudokuSolver
+{
+    public class BasicBacktrackingSolver<T> : ISolver<T>
+    {
+        public void Solve(SudokuBoard<T> board)
+        {
+            SolveSquare(board, 0);
+        }
+
+        private void SolveSquare(SudokuBoard<T> board, int pos)
+        {
+            int boardWidth = board.BlockSideLength * board.BlockSideLength;
+
+            if (pos >= boardWidth * boardWidth)
+                return;
+
+            if (board[pos / boardWidth, pos % boardWidth].Equals(board.EmptyValue))
+            {
+                HashSet<T> possibilities = board.LegalValues.ToHashSet();
+
+                // The number of row of the current block (for a 9 X 9 board can be 0, 1 or 2. if pos was 35 for example, it will be 1 (because second block-row))
+                int blockRow = pos / (boardWidth * board.BlockSideLength);
+
+                // The number of column of the current block (for a 9 X 9 board can be 0, 1 or 2. if pos was 35 for example, it will be 2 (because third block-column))
+                int blockColumn = ((pos - blockRow) % boardWidth) / board.BlockSideLength; 
+
+                // TODO: finish!
+                for (int i = 0; i < boardWidth; i++)
+                {
+                    
+                }
+            }
+
+            SolveSquare(board, pos + 1);
+        }
+    }
+}
---
+++ src/Solvers/ISolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudokuSolver
+{
+    /// <summary>
+    /// Interface for Sudoku solving algorithms.
+    /// </summary>
+    /// <typeparam name="T">The type of data at each square of the board.</typeparam>
+    public interface ISolver<T>
+    {
+        /// <summary>
+        /// Solve the Sudoku board by filling in all of the missing values.
+        /// </summary>
+        /// <param name="board">The board to solve.</param>
+        public void Solve(SudokuBoard<T> board);
+    }
+}
