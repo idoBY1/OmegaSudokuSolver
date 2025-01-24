c305c77b2f4ee27a8c49f10ba42e4eb77ba7b212
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-01-25 01:06:21 +0200

    refactor: added documentation for RuleBasedSolver

--- src/Solvers/RuleBasedSolver.cs
+++ src/Solvers/RuleBasedSolver.cs
@@ -17,10 +17,19 @@ namespace OmegaSudokuSolver
             return RuleBasedBackTrack(new SudokuBoard<T>(board), notes);
         }
 
+        /// <summary>
+        /// The recursive function for solving the board. This function apllies the rules <br />
+        /// to the board and then solves the board by filling in the square with the least <br />
+        /// possible options first.
+        /// </summary>
+        /// <param name="board">The board to solve.</param>
+        /// <param name="notes">Dictionary for storing the possible values of each square of the board.</param>
+        /// <returns>The solved board. Returns null if failed to solve the board.</returns>
         private SudokuBoard<T> RuleBasedBackTrack(SudokuBoard<T> board, Dictionary<int, HashSet<T>> notes)
         {
             bool updated = true;
 
+            // Apply rules.
             while (updated)
             {
                 updated = false;
@@ -30,8 +39,10 @@ namespace OmegaSudokuSolver
                 updated = ApplySingles(board, notes) || ObviousTuples(board, notes);
             }
 
+            // Find the square with the least possibilities.
             int pos = SetSolveUtils.FindLeastNotesIndex(notes);
 
+            // Check if finished.
             if (checker.IsFull(board) || pos == -1)
             {
                 if (checker.IsSolved(board))
@@ -40,6 +51,7 @@ namespace OmegaSudokuSolver
                     return null;
             }
 
+            // Try all of the possible values for this square using backtracking.
             foreach (T val in notes[pos])
             {
                 notes.Remove(pos);
@@ -55,6 +67,11 @@ namespace OmegaSudokuSolver
             return null;
         }
 
+        /// <summary>
+        /// Removes from the notes all of the values that already apear in the group elsewhere on the board.
+        /// </summary>
+        /// <param name="board">The board to read from.</param>
+        /// <param name="notes">The notes to change based on the current state of the board.</param>
         private void ClearTrivialNotes(SudokuBoard<T> board, Dictionary<int, HashSet<T>> notes)
         {
             foreach (var note in notes)
@@ -83,6 +100,12 @@ namespace OmegaSudokuSolver
             }
         }
 
+        /// <summary>
+        /// Write to the board all of the values in the squares with only one possibility left.
+        /// </summary>
+        /// <param name="board">The board to write to.</param>
+        /// <param name="notes">The notes to read and change.</param>
+        /// <returns>'true' if the function changed the board and 'false' if it didn't</returns>
         private bool ApplySingles(SudokuBoard<T> board, Dictionary<int, HashSet<T>> notes)
         {
             bool updated = false;
@@ -92,6 +115,7 @@ namespace OmegaSudokuSolver
                 if (note.Value.Count == 0)
                     return false;
 
+                // If only one possibility left for this square, write this value to the board.
                 if (note.Value.Count == 1)
                 {
                     board.Set(note.Key, note.Value.ElementAt(0));
@@ -103,8 +127,16 @@ namespace OmegaSudokuSolver
             return updated;
         }
 
+        /// <summary>
+        /// Apllies the rules of obvious tuples to the notes. This function clears notes <br/>
+        /// from squares if they are not possible based on other squares in the same group. <br/>
+        /// </summary>
+        /// <param name="board">The board to read.</param>
+        /// <param name="notes">The notes to change.</param>
+        /// <returns>'true' if the function changed the notes and 'false' if it didn't.</returns>
         private bool ObviousTuples(SudokuBoard<T> board, Dictionary<int, HashSet<T>> notes)
         {
+            // A list to store the positions of squares in the current group.
             List<int> groupIndexes = new List<int>();
 
             bool updated = false;
@@ -112,11 +144,13 @@ namespace OmegaSudokuSolver
             // Perform on blocks
             for (int i = 0; i < board.Width; i++)
             {
+                // Reset groupIndexes for the next group.
                 groupIndexes.Clear();
 
                 int blockRow = i / board.BlockSideLength;
                 int blockColumn = i % board.BlockSideLength;
 
+                // Add all positions in the current block.
                 for (int j = 0; j < board.Width; j++)
                 {
                     int row = blockRow * board.BlockSideLength + (j / board.BlockSideLength);
@@ -126,50 +160,73 @@ namespace OmegaSudokuSolver
                         groupIndexes.Add(row * board.Width + col);
                 }
 
+                // Apply the rule on this group.
                 updated = updated || ObviousTuplesInGroup(board, notes, groupIndexes);
             }
 
             // Perform on rows
             for (int i = 0; i < board.Width; i++)
             {
+                // Reset groupIndexes for the next group.
                 groupIndexes.Clear();
 
+                // Add all positions in the current row.
                 for (int j = 0; j < board.Width; j++)
                 {
                     if (board[i, j].Equals(board.EmptyValue))
                         groupIndexes.Add(i * board.Width + j);
                 }
 
+                // Apply the rule on this group.
                 updated = updated || ObviousTuplesInGroup(board, notes, groupIndexes);
             }
 
             // Perform on columns
             for (int i = 0; i < board.Width; i++)
             {
+                // Reset groupIndexes for the next group.
                 groupIndexes.Clear();
 
+                // Add all positions in the current column.
                 for (int j = 0; j < board.Width; j++)
                 {
                     if (board[j, i].Equals(board.EmptyValue))
                         groupIndexes.Add(j * board.Width + i);
                 }
 
+                // Apply the rule on this group.
                 updated = updated || ObviousTuplesInGroup(board, notes, groupIndexes);
             }
 
             return updated;
         }
 
+        /// <summary>
+        /// Applies the rules of obvious tuples on a group of squares (row / column / block). <br/>
+        /// The function checks different combinations of the squares and if it finds a <br/>
+        /// combination that has the same number of possible values as the number of squares <br/>
+        /// in the combination, it clears the possible values of the combination from the <br/>
+        /// notes of every square that is not in the combination because the values must be <br/>
+        /// assigned to a square from the combination (or else there will be a square without <br/>
+        /// a value, which is not possible).
+        /// </summary>
+        /// <param name="board">The board to read.</param>
+        /// <param name="notes">The notes to change.</param>
+        /// <param name="group">The list of the possitions of every square in the group that has not already been filled.</param>
+        /// <returns>'true' if the function changed the notes and 'false' if it didn't.</returns>
         private bool ObviousTuplesInGroup(SudokuBoard<T> board, Dictionary<int, HashSet<T>> notes, List<int> group)
         {
             bool updated = false;
 
+            // For every combination size.
             for (int combinationSize = 2; combinationSize < group.Count; combinationSize++)
             {
                 HashSet<HashSet<int>> combinations = SetSolveUtils.GetCombinations(group, combinationSize);
 
+                // Check every combination.
                 foreach (HashSet<int> combination in combinations)
                 {
+                    // Stores all of the possibilities of every square in the combination.
                     var combinationValues = new HashSet<T>();
 
                     // Add all of the possible values of every square in this combination to the combinationValues set.
