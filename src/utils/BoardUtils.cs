098add83587c33c62bbf64fd632435b632a68bd8
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-01-25 15:18:58 +0200

    test: created checker test

---
+++ src/utils/BoardUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudokuSolver
+{
+    public class BoardUtils
+    {
+        public static SudokuBoard<int> CreateBoardFromString(string boardString)
+        {
+            int blockLength = (int)Math.Sqrt(Math.Sqrt(boardString.Length));
+
+            var board = new SudokuBoard<int>(blockLength, Enumerable.Range(1, blockLength * blockLength), 0);
+
+            for (int i = 0; i < boardString.Length; i++)
+            {
+                board.Set(i, int.Parse(boardString[i].ToString()));
+            }
+
+            return board;
+        }
+    }
+}
